771146d097059f576b3e96df1d9704800134c9db
Harsh Singh <harshsinghinf@gmail.com>
2024-11-18 19:26:47 +0530

    Time: 52 ms (5.05%), Space: 9.3 MB (35.56%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.empty()) return true;
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        for(auto i=s.begin();i<s.end();i++){
+            //if(97<=int(*i) && int(*i)<=122){
+            if(isalnum(*i)){
+                continue;
+            }
+            else if(*i == ' '){ s.erase(i); i--;}
+            else{
+                s.erase(i);
+                i--;
+            }
+        }
+        string revs = s;
+        reverse(revs.begin(),revs.end());
+        cout<<revs<<endl<<s<<endl;
+        return (s==revs);
+    }
+};
