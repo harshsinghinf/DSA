46c99b1cb88686f8ff325c107fda58dfcbc01648
Harsh Singh <harshsinghinf@gmail.com>
2026-09-16 22:42:49 +0530

    Time: 52 ms (13.01%), Space: 19.3 MB (18.99%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -2,7 +2,7 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         m: int = len(word1)
         n: int = len(word2)
-        x: int = m if m>=n else n
+        x: int = max(m,n)
         res: str = ""
         for i in range(x):
             if i<m:
